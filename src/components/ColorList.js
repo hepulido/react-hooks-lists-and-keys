bde2929337273e3f47a45b26d81969b326200eb6
hector <hpulido1021@gmail.com>
2022-06-01 18:50:18 -0700

    Completed assignment

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -8,6 +8,19 @@ function ColorList() {
     "darkslategray",
     "hotpink",
   ];
+  const colorElements = colors.map((color) => {
+    return <li key={color} style={{ color: color }}>{color}</li>;
+  });
+  return (
+    <div>
+      <h1>Top 5 CSS Colors</h1>
+      <ol>
+        {/* display the array of <li> elements here! */}
+        {colorElements}
+      </ol>
+    </div>
+  );
+}
 
   return (
     <div>
